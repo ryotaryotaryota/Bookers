eb7d326e9ee8f1af75074a4999e83ddc7676b25d
ryota takahashi <ryota0911@gmail.com>
2020-10-19 20:05:26 +0900

    like.controllerのいらんやつ削除

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -14,21 +14,21 @@ class LikesController < ApplicationController
 end
 
 
-@currentUserEntry=Entry.where(user_id: current_user.id)
-@userEntry=Entry.where(user_id: @user.id)
-if @user.id == current_user.id
-else
-  @currentUserEntry.each do |cu|
-    @userEntry.each do |u|
-      if cu.room_id == u.room_id then
-        @isRoom = true
-        @roomId = cu.room_id
-      end
-    end
-  end
-  if @isRoom
-  else
-    @room = Room.new
-    @entry = Entry.new
-  end
-end
+# @currentUserEntry=Entry.where(user_id: current_user.id)
+# @userEntry=Entry.where(user_id: @user.id)
+# if @user.id == current_user.id
+# else
+#   @currentUserEntry.each do |cu|
+#     @userEntry.each do |u|
+#       if cu.room_id == u.room_id then
+#         @isRoom = true
+#         @roomId = cu.room_id
+#       end
+#     end
+#   end
+#   if @isRoom
+#   else
+#     @room = Room.new
+#     @entry = Entry.new
+#   end
+# end
